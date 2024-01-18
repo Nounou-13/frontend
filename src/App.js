4952c43afd28450656063bcdc0d2cd0db08cb592
Nouredine  Mahaman Laminou Dicko <nouredinelamine13@gmail.com>
2024-01-18 13:39:01 +0000

    Changement.01

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import AppLayout from './components/AppLayout';
 import AppNavbar from './components/AppNavbar';
 import AirportList from './components/AirportList';
